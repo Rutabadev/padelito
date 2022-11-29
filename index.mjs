3bd357dd43051b7649269d5050a8e833b935a425
Rutabadev <etienner37@gmail.com>
2022-11-29 21:51:22 +0100

    remove useless option

--- index.mjs
+++ index.mjs
@@ -3,7 +3,6 @@ import cypress from 'cypress';
 export const handler = ({ horaire = "18:00" }) => {
    cypress.run({
       spec: "cypress/e2e/padelito/book.cy.js",
-      browser: "chrome",
       env: {
          "HORAIRE": horaire,
       }
