6d46c83c3015b79240e9883b8bd9a6fc27fe83dd
Etienne Raby <etienne.raby@fittingbox.com>
2022-12-14 01:19:24 +0100

    working deployement (try docker + curl local on windows)

--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
 import chromium from "@sparticuz/chromium";
 
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
